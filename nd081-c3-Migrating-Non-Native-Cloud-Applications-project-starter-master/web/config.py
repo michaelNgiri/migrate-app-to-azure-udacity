7224f9701a9fa61c52c69924a2c472155e045a85
Michael Ngiri <michaelngiri@gmail.com>
2020-05-24 02:52:39 +0100

    upload project to github

---
+++ nd081-c3-Migrating-Non-Native-Cloud-Applications-project-starter-master/web/config.py
@@ -0,0 +1,25 @@
+import os
+ 
+app_dir = os.path.abspath(os.path.dirname(__file__))
+
+class BaseConfig:
+    DEBUG = True
+    POSTGRES_URL="udacaity-migrate-to-azure-db.postgres.database.azure.com"
+    POSTGRES_USER="techconf@udacaity-migrate-to-azure-db"
+    POSTGRES_PW="Alveis.101" 
+    POSTGRES_DB="techconfdb"
+    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
+    CONFERENCE_ID = 1
+    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm' 
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://udacity-migrate-to-azure-notificationqueue.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=IShkCuMwrKGEGLGLX0KMKwXfhGy8r2Qn4U63pGt4ySU='
+    SERVICE_BUS_QUEUE_NAME ='notificationqueue'
+    ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
+    SENDGRID_API_KEY = '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"'
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
